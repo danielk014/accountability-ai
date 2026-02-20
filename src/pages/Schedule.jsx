e2be61d9838ab68aa945bfeb6768a713c9aa2da7
Daniel Kusakci <danielkcibusiness@gmail.com>
2026-02-20 20:25:21 +0000

    File changes

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -25,6 +25,7 @@ export default function Schedule() {
   });
 
   const today = format(new Date(), "yyyy-MM-dd");
+  const [showForm, setShowForm] = useState(false);
 
   const toggleCompletionMutation = useMutation({
     mutationFn: async ({ task, date }) => {
