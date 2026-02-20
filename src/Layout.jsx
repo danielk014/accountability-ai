fcc9f32e2cf70ec27236e0391e6362fb162a3ae2
Daniel Kusakci <danielkcibusiness@gmail.com>
2026-02-20 20:07:14 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { base44 } from "@/api/base44Client";
 import { Link } from "react-router-dom";
 import { createPageUrl } from "./utils";
 import { LayoutDashboard, MessageCircle, BarChart3, ListChecks, CalendarDays, Shield } from "lucide-react";
